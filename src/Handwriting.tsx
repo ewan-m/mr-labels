9377a17e92e1afb8e69106fe924c313fadf5ab13
ewan-m <ewanmorrrison@gmail.com>
2024-10-04 15:44:19 +0100

    feat: initial commit

--- src/Handwriting.tsx
+++ src/Handwriting.tsx
@@ -1,92 +1,191 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useEffect, useRef, useState } from "react";
+import "./HandwritingMeasurement.css";
+import imagesMap from './images.json';
 
-type Coord = {x: number, y: number};
+const measurementTypes = [
+  "Slant",
+  "TopToMid",
+  "MidToBase",
+  "BaseToBottom",
+  "LetterWidth",
+] as const;
+type MeasurementType = (typeof measurementTypes)[number];
+const descriptionTypes = [
+  "Angularity",
+  "Loopiness",
+  "ConnectionFluidity",
+  "LetterSpacing",
+] as const;
+const measurementColorMap: Record<MeasurementType, string> = {
+  Slant: "red",
+  TopToMid: "blue",
+  MidToBase: "green",
+  BaseToBottom: "brown",
+  LetterWidth: "purple",
+};
+
+type Coord = { x: number; y: number };
 
 type Measurement = {
   start: Coord;
   end: Coord;
-  type: "Slant" | "TopToMid" | "MidToBase" | "BaseToBottom" | "LetterSpacing";
-}
+  type: MeasurementType;
+};
 
-const HandwritingMeasurement: React.FC<{imageUrl: string}> = ({ imageUrl }) => {
+const HandwritingMeasurement = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const authors = Object.keys(imagesMap);
+
+  const [authorIndex, setAuthorIndex] = useState(0);
+
+
   const [measurements, setMeasurements] = useState<Measurement[]>([]);
-  const [startPosition, setStartPosition] = useState<Coord>({x: 0, y: 0});
-  const [currentPosition, setCurrentPosition] = useState<Coord>({x: 0, y: 0});
+  const [descriptionScores, setDescriptionScores] = useState<
+    Record<(typeof descriptionTypes)[number], string>
+  >({
+    Angularity: "0.0",
+    Loopiness: "0.0",
+    ConnectionFluidity: "0.0",
+    LetterSpacing: "0.0",
+  });
 
   const [measuring, setMeasuring] = useState<Measurement["type"]>("Slant");
+  const [startPosition, setStartPosition] = useState<Coord | null>(null);
 
   useEffect(() => {
-
     const canvas = canvasRef.current;
-    if (canvas === null) {
-      return;
-    }
+    if (canvas === null) return;
+
     const img = new Image();
-    img.src = imageUrl;
+    img.src = `/${(imagesMap as Record<string, string[]>)[authors[authorIndex]][0]}.png`;
 
     img.onload = () => {
-      canvas.width = img.width;
-      canvas.height = img.height;    
-      const ctx = canvas.getContext('2d');
-      if (ctx === null) {
-        return;
-      }
+      canvas.width = 800;
+      canvas.height = (800 / img.width) * img.height;
+      const ctx = canvas.getContext("2d");
+      if (ctx === null) return;
 
-      ctx.drawImage(img, 0, 0, img.width, img.height);
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
     };
-  }, [imageUrl]);
+  }, [authorIndex, authors]);
 
-  const drawMeasurement = ({ startX, startY, endX, endY }) => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
-
-    // Draw line for the measurement
+  const drawLine = (
+    ctx: CanvasRenderingContext2D,
+    start: Coord,
+    end: Coord,
+  ) => {
     ctx.beginPath();
-    ctx.moveTo(startX, startY);
-    ctx.lineTo(endX, endY);
-    ctx.strokeStyle = 'red';
+    ctx.moveTo(start.x, start.y);
+    ctx.lineTo(end.x, end.y);
+    ctx.strokeStyle = measurementColorMap[measuring];
     ctx.lineWidth = 2;
     ctx.stroke();
-
   };
 
+  const handleMouseDown = (e: React.MouseEvent<HTMLCanvasElement>) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    setStartPosition({ x, y });
+  };
 
+  const handleMouseUp = (e: React.MouseEvent<HTMLCanvasElement>) => {
+    if (!startPosition) return;
+
+    const rect = e.currentTarget.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    const endPosition = { x, y };
+
+    setMeasurements((m) => [
+      ...m,
+      { start: startPosition, end: endPosition, type: measuring },
+    ]);
+    setStartPosition(null);
+
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const ctx = canvas.getContext("2d");
+      if (ctx) {
+        drawLine(ctx, startPosition, endPosition);
+      }
+    }
+  };
+
+  const jsonString = JSON.stringify(
+    { measurements: measurements, descriptionScores },
+    null,
+    2,
+  );
+
+  const downloadJson = () => {
+    const blob = new Blob([jsonString], { type: "application/json" });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = `measurements-${authors[authorIndex]}.json`;
+    a.click();
+
+    // Clean up the URL object
+    URL.revokeObjectURL(url);
+  };
 
   return (
-    <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
-      <h3>Handwriting Measurement Tool</h3>
-      <select value={measuring} onChange={(e) => {
-        setMeasuring(e.target.value as Measurement["type"]);
-      }}>
-        <option>Slant</option>
-        <option>TopToMid</option>
-        <option>MidToBase</option>
-        <option>BaseToBottom</option>
-        <option>LetterSpacing</option>
-      </select>
+    <div className="container">
+      <h3 className="title">Handwriting Measurement Tool</h3>
+      <p className="info">
+        A tool to provide more human insight into my author similarity matchy
+        matchy algorithm
+      </p>
+      <div className="inputs">
+        {descriptionTypes.map((type) => (
+          <label key={type} className="label">
+            {type}{" "}
+            <input
+              className="input"
+              value={descriptionScores[type]}
+              type="text"
+              onChange={(e) => {
+                setDescriptionScores((c) => ({ ...c, [type]: e.target.value }));
+              }}
+            />{" "}
+          </label>
+        ))}
+      </div>
+      <div className="buttons">
+        {measurementTypes.map((type) => (
+          <button
+            type="button"
+            onClick={(e) => {
+              e.preventDefault();
+              setMeasuring(type);
+            }}
+            key={type}
+            className={`button ${measuring === type ? "active" : ""}`}
+          >
+            {type}
+          </button>
+        ))}
+      </div>
       <canvas
         ref={canvasRef}
-        onMouseDown={(e) => {
-          const rect = e.currentTarget.getBoundingClientRect();
-          const x = e.clientX - rect.left;
-          const y = e.clientY - rect.top;
-          setMeasurements(m => [...m, {start: startPosition, end: {x,y}, type :measuring}])
-        }}
-
-        onMouseMove={(e) => {
-          const rect = e.currentTarget.getBoundingClientRect();
-          const x = e.clientX - rect.left;
-          const y = e.clientY - rect.top;
-          setCurrentPosition({x,y})
-        }}
-        onMouseUp={(e) => {
-          const rect = e.currentTarget.getBoundingClientRect();
-          const x = e.clientX - rect.left;
-          const y = e.clientY - rect.top;
-          setStartPosition({x,y})
-        }}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        className="canvas"
       />
+      <div className="action-buttons">
+        <button className="button" onClick={downloadJson}>
+          Download
+        </button>
+        <button onClick={(e) => {
+          e.preventDefault();
+          setAuthorIndex(a => a+1);
+        }} className="button">Next</button>
+      </div>
+      <pre className="json-output">{jsonString}</pre>
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-import Handwriting from './Handwriting'
+import Handwriting from "./Handwriting";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Handwriting imageUrl="https://www.researchgate.net/profile/Karl-Heinz-Steinke/publication/224082148/figure/fig1/AS:393986553466881@1470945122518/Handwriting-from-IAM-database.png"/>
+    <Handwriting />
   </StrictMode>,
-)
+);
